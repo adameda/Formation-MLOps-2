c49030c049c0094b67d3d4675f942774a7214180
adameda <adamhalladja@yahoo.fr>
2026-01-29 15:25:19 +0000

    MLflow job

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -17,12 +17,16 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     target = 'Ba_avg'
     X = features.drop(columns=[target])
     y = features[target]
+
     with mlflow.start_run():
-        # insert autolog here ...
+        mlflow.sklearn.autolog()  # activation du logging automatique
+
         model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
         model.fit(X, y)
+
     time_str = time.strftime('%Y%m%d-%H%M%S')
     joblib.dump(model, os.path.join(model_registry_folder, time_str + '.joblib'))
+
 
 
 def predict_with_io(features_path: str, model_path: str, predictions_folder: str) -> None:
